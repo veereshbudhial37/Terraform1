e881a651062fbd43d56120c6bc5f41fe08da54c3
veeresh <veereshbudhial37@gmail.com>
2023-03-16 16:39:29 +0530

    fgf

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
 resource "aws_instance" "my_insatancce1" {
-    ami = "ami-02238ac43d6385ab3"
-    instance_type = "t2.micro"
+    ami = var.ami_id
+    instance_type = var.ec2_type
     tags = {
       Name = "terraform"
     }
   
-}
-
-
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "mebuc9111"
-
-  tags = {
-    Name        = "My bucket1"
-    Environment = "Dev"
-  }
 }
